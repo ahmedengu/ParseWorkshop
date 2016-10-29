134f21f231c17f0bbf40b68e3f502c2d2acd10b0
Ahmed Kamel Taha <ahmedengu@gmail.com>
2016-10-29 03:36:46 +0200

    removing active records

--- TodoRails/config/application.rb
+++ TodoRails/config/application.rb
@@ -1,6 +1,11 @@
 require_relative 'boot'
 
-require 'rails/all'
+require "action_controller/railtie"
+require "action_view/railtie"
+require "action_mailer/railtie"
+require "active_job/railtie"
+require "action_cable/engine"
+require "sprockets/railtie"
 require 'parse-ruby-client'
 
 # Require the gems listed in Gemfile, including any gems
